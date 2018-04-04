d1a8f07fbaa2cc46aa814fcbf70340dd1454d4d5
Adiel <32271159+MatufA@users.noreply.github.com>
2018-04-05 01:29:06 +0300

    Update Member.h
    Improving destractor

--- Member.h
+++ Member.h
@@ -19,9 +19,12 @@ class Member{
 
     ~Member(){
       map<unsigned int, Member*>::iterator it;
+      for ( it = following.begin(); it != following.end(); it++)
+        unfollow(*(it-> second));
 
-      for ( it = following.begin(); it != following.end(); it++ )
-        unfollow(*it);
+      for ( it = followed_by.begin(); it != followed_by.end(); it++)
+	      (it-> second)-> erase_following(n_ID);
+
       num_of_mem--; }
 
     unsigned int get_ID() {return n_ID;}
@@ -39,6 +42,8 @@ class Member{
     void add_followed(Member &obj){ followed_by.insert(pair<unsigned int, Member*>(obj.get_ID(), &obj)); }
 
     void erase_followed(unsigned int id){ followed_by.erase(id); }
+
+    void erase_following(unsigned int id){ following.erase(id); }
 
     static unsigned int count(void){ return num_of_mem;}
 };
