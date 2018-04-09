b15beae0e53b763c78bba5d2c1eb85391a239376
Yehuda Neumann <33513480+Yuda4@users.noreply.github.com>
2018-04-09 18:53:51 +0300

    Update

--- Member.h
+++ Member.h
@@ -12,6 +12,10 @@ class Member{
     map <unsigned int, Member*> followed_by; //who is following me
     map <unsigned int, Member*> following;//following after
 
+    void add_followed(Member &obj){ followed_by.insert(pair<unsigned int, Member*>(obj.get_ID(), &obj)); }
+    void erase_followed(unsigned int id){ followed_by.erase(id); }
+    void erase_following(unsigned int id){ following.erase(id); }
+
     public:
     Member(){
       n_ID = number_id++;
@@ -32,21 +36,17 @@ class Member{
     int numFollowing() { return following.size();}
 
     void follow(Member &obj) {
-        if(obj.get_ID() != this->n_ID){
-        obj.add_followed(*this);
-        following.insert(pair<unsigned int, Member*>(obj.get_ID(), &obj));
+      if(obj.get_ID() != this->n_ID ){
+          obj.add_followed(*this);
+          following.insert(pair<unsigned int, Member*>(obj.get_ID(), &obj));
       }
     }
 
     void unfollow(Member &obj) {
         obj.erase_followed(n_ID);
-        following.erase(obj.get_ID());}
-
-    void add_followed(Member &obj){ followed_by.insert(pair<unsigned int, Member*>(obj.get_ID(), &obj)); }
-
-    void erase_followed(unsigned int id){ followed_by.erase(id); }
-
-    void erase_following(unsigned int id){ following.erase(id); }
+        following.erase(obj.get_ID());
+    }
 
     static unsigned int count(void){ return num_of_mem;}
+    
 };
