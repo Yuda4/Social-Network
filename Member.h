eac56e2b2e4f795c8a59583cf7e01f451db27131
Yehuda Neumann <33513480+Yuda4@users.noreply.github.com>
2018-04-09 19:15:18 +0300

    Update
    adding documentation

--- Member.h
+++ Member.h
@@ -9,19 +9,19 @@ class Member{
 
     private:
     unsigned int n_ID;
-    map <unsigned int, Member*> followed_by; //who is following me
-    map <unsigned int, Member*> following;//following after
+    map <unsigned int, Member*> followed_by; // map for who is following me
+    map <unsigned int, Member*> following;// map for who i'm following after
 
-    void add_followed(Member &obj){ followed_by.insert(pair<unsigned int, Member*>(obj.get_ID(), &obj)); }
-    void erase_followed(unsigned int id){ followed_by.erase(id); }
-    void erase_following(unsigned int id){ following.erase(id); }
+    void add_followed(Member &obj){ followed_by.insert(pair<unsigned int, Member*>(obj.get_ID(), &obj)); } // adding me to member's map of followed_by
+    void erase_followed(unsigned int id){ followed_by.erase(id); }  // helping function - erasing me of member's map of followed_by
+    void erase_following(unsigned int id){ following.erase(id); }  // helping destructor to erase me of all members maps of followed_by
 
     public:
-    Member(){
+    Member(){  // Constructor
       n_ID = number_id++;
       num_of_mem++;}
 
-    ~Member(){
+    ~Member(){  // Destructor
       map<unsigned int, Member*>::iterator it;
       for ( it = following.begin(); it != following.end(); it++)
         unfollow(*(it-> second));
@@ -31,22 +31,22 @@ class Member{
 
       num_of_mem--; }
 
-    unsigned int get_ID() {return n_ID;}
-    int numFollowers() { return followed_by.size();}
-    int numFollowing() { return following.size();}
+    unsigned int get_ID() {return n_ID;} // gives my id number
+    int numFollowers() { return followed_by.size();}  // gives map's size of how many members are following me
+    int numFollowing() { return following.size();}  // gives map's size of how many members i'm following after
 
-    void follow(Member &obj) {
+    void follow(Member &obj) {  // follow after another member
       if(obj.get_ID() != this->n_ID ){
           obj.add_followed(*this);
           following.insert(pair<unsigned int, Member*>(obj.get_ID(), &obj));
       }
     }
 
-    void unfollow(Member &obj) {
+    void unfollow(Member &obj) {  // unfollow a member
         obj.erase_followed(n_ID);
         following.erase(obj.get_ID());
     }
 
-    static unsigned int count(void){ return num_of_mem;}
+    static unsigned int count(void){ return num_of_mem;}  // function gives how many members are active
     
 };
